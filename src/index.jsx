1718665a696900c9ca79d798ad4b1f69a32b54fe
Attilio <attilio@>
2018-11-21 15:41:53 +0100

    Configure Babel and Webpack

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,10 @@
-console.log("it works")
+import React from 'react';
+import {render} from 'react-dom';
 
+class App extends React.Component {
+  render () {
+    return <p> It woks!</p>;
+  }
+}
+
+render(<App/>, document.getElementById('app'));
