3b34be7c3bf4fd846421d1ce08669585ba4be824
Attilio <attilio@>
2018-11-25 23:57:27 +0100

    example with React.createElement

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import {TestComponent} from './TestComponent.jsx'
+
 class App extends React.Component {
   render () {
-    return <p> Hello World!</p>;
+    const testComponent = React.createElement(TestComponent, {text: 'Hello, world (React.createElement)'}, null)
+    return [
+       <div>Hello, world</div>,
+       testComponent
+    ];
   }
 }
 
