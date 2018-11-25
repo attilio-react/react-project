2417756a104e57e86c31caf947e63cfddaec2c13
Attilio <attilio@>
2018-11-26 00:17:08 +0100

    example with Functional Component

---
+++ src/TestFunctionalComponent.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TestFunctionalComponent = ({text}) => <div>{text} (Functional Component)</div>
+
+export {TestFunctionalComponent}
--- src/index.jsx
+++ src/index.jsx
@@ -3,17 +3,20 @@ import {render} from 'react-dom';
 
 import {TestComponent} from './TestComponent.jsx'
 import {TestPureComponent} from './TestPureComponent.jsx'
+import {TestFunctionalComponent} from './TestFunctionalComponent.jsx'
 
 class App extends React.Component {
   render () {
     const testComponent = React.createElement(TestComponent, {text: 'Hello, world (React.createElement)'}, null),
           testComponent2 = <TestComponent text='Hello, world (React.Component)' />,
-          testPureComponent = <TestPureComponent text='Hello, world' />
+          testPureComponent = <TestPureComponent text='Hello, world' />,
+          testFunctionalComponent = <TestFunctionalComponent text='Hello, world' />
     return [
        <div>Hello, world</div>,
        testComponent,
        testComponent2,
-       testPureComponent
+       testPureComponent,
+       testFunctionalComponent
     ];
   }
 }
