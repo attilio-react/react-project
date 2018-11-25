0612508301662c38e6c38312936d379e316ab4fa
Attilio <attilio@>
2018-11-25 23:59:59 +0100

    example with React.Component

--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,12 @@ import {TestComponent} from './TestComponent.jsx'
 
 class App extends React.Component {
   render () {
-    const testComponent = React.createElement(TestComponent, {text: 'Hello, world (React.createElement)'}, null)
+    const testComponent = React.createElement(TestComponent, {text: 'Hello, world (React.createElement)'}, null),
+          testComponent2 = <TestComponent text='Hello, world (React.Component)' />
     return [
        <div>Hello, world</div>,
-       testComponent
+       testComponent,
+       testComponent2
     ];
   }
 }
