2eb831f167b02a3cc8a7d95a47c72729669c472b
A <parrtochaffinc@gmail.com>
2018-12-03 21:25:24 +0100

    NoResult

---
+++ src/NoResult.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class NoResult extends React.PureComponent {
+  render () {
+    return <div style={{fontSize: 'large'}}>No films found</div>;
+  }
+}
+
+
+export {NoResult}
+
+
--- src/SearchResult.jsx
+++ src/SearchResult.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import {NoResult} from './NoResult.jsx'
+
 class SearchResult extends React.PureComponent {
   render () {
-    return <div>Search result</div>;
+    const {results} = this.props,
+          resultsFound = results && results.length > 0;
+    return resultsFound ? <div>Search result</div> : <NoResult/>;
   }
 }
 
