7b3fb732c9298138ee9745e3aa97a71c0bba5009
A <parrtochaffinc@gmail.com>
2019-01-19 18:21:05 +0100

    Test fetching movies by genre

--- test/ApiOperations.spec.jsx
+++ test/ApiOperations.spec.jsx
@@ -3,10 +3,25 @@ import {getMoviesByGenres} from '|ApiOperations.jsx';
 const EXCLUDE_ID = 42
 
 describe('Getting movies based on genres', () => {
+      beforeEach(() => {
+          fetch.resetMocks()
+      });
+
       it('Zero genres should return no movies', () => {
           const doneCallback = jest.fn()
           getMoviesByGenres([], EXCLUDE_ID, doneCallback)
           expect(doneCallback).toBeCalledWith([])
       });
+
+      it('One genre should return a result', (done) => {
+          const RESPONSE = {"data":[{"id":1,"title":"Title","tagline":"Tagline","vote_average":6.1,"vote_count":1195,"release_date":"2018-02-07","poster_path":"http://path/to/poster.jpg","overview":"OVERVIEW","budget":1,"revenue":2,"genres":["Drama"],"runtime":3}],"total":1,"offset":0,"limit":10}
+ 
+          fetch.mockResponseOnce(JSON.stringify(RESPONSE))
+          getMoviesByGenres(['drama'], EXCLUDE_ID, (result) => {
+              expect(result).toEqual(RESPONSE.data)
+              done()
+          })
+      });
+
 });
 
--- test/testSetup.jsx
+++ test/testSetup.jsx
@@ -4,5 +4,7 @@ import * as enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 
+fetch = require('jest-fetch-mock')
+
 enzyme.configure({adapter: new Adapter()});
 
