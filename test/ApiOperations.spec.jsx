3b8a51abdc45648767bb1e47a832d3e4e279d95b
A <parrtochaffinc@gmail.com>
2019-01-19 18:32:38 +0100

    Test for getMovie

--- test/ApiOperations.spec.jsx
+++ test/ApiOperations.spec.jsx
@@ -1,6 +1,8 @@
-import {getMoviesByGenres} from '|ApiOperations.jsx';
+import {getMovie, getMoviesByGenres} from '|ApiOperations.jsx';
 
-const EXCLUDE_ID = 42
+const EXCLUDE_ID = 42,
+      MOVIE_ID = 43,
+      RESPONSE = {"data":[{"id":1,"title":"Title","tagline":"Tagline","vote_average":6.1,"vote_count":1195,"release_date":"2018-02-07","poster_path":"http://path/to/poster.jpg","overview":"OVERVIEW","budget":1,"revenue":2,"genres":["Drama"],"runtime":3}],"total":1,"offset":0,"limit":10}
 
 describe('Getting movies based on genres', () => {
       beforeEach(() => {
@@ -14,8 +16,6 @@ describe('Getting movies based on genres', () => {
       });
 
       it('One genre should return a result', (done) => {
-          const RESPONSE = {"data":[{"id":1,"title":"Title","tagline":"Tagline","vote_average":6.1,"vote_count":1195,"release_date":"2018-02-07","poster_path":"http://path/to/poster.jpg","overview":"OVERVIEW","budget":1,"revenue":2,"genres":["Drama"],"runtime":3}],"total":1,"offset":0,"limit":10}
- 
           fetch.mockResponseOnce(JSON.stringify(RESPONSE))
           getMoviesByGenres(['drama'], EXCLUDE_ID, (result) => {
               expect(result).toEqual(RESPONSE.data)
@@ -23,5 +23,12 @@ describe('Getting movies based on genres', () => {
           })
       });
 
+      it('Movies should be fetched by ID', (done) => {
+          fetch.mockResponseOnce(JSON.stringify(RESPONSE.data))
+          getMovie(MOVIE_ID, (result) => {
+              expect(result).toEqual(RESPONSE.data)
+              done()
+          })
+      });
 });
 
