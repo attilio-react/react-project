2e1204c4649bdd4de04fae63d5fd88740c5aea58
Attilio <attilio@>
2019-01-22 17:27:06 +0100

    ResultItem as Link

--- src/common/ResultItem.jsx
+++ src/common/ResultItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 import {Label} from 'Common/Label.jsx';
 import {Button} from 'Common/Button.jsx';
@@ -8,7 +9,7 @@ class ResultItem extends React.PureComponent {
  render () {
     const {item, clickCallback} = this.props,
           self = this;
-    return <div style={{border: '1px solid black'}}><a onClick={() => {clickCallback(item.id)}}>
+    return <div style={{border: '1px solid black'}}><Link to={`/details/${item.id}`}>
                   <p><img src={item.poster_path} /></p>
                   <p>
 		     <Label text={item.release_date} />
@@ -16,7 +17,7 @@ class ResultItem extends React.PureComponent {
                      <Label text={item.genres.join(', ')} />
 		  </p>
                   <p><Label text={item.title} /></p>
-                 </a> 
+                 </Link> 
            </div>;
   }
 }
