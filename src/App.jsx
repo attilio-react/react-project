369df27a5811a8dc6561d2b90b9516ca406b85d2
A <parrtochaffinc@gmail.com>
2019-01-27 16:10:43 +0100

    Use switch for exactly one route

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {paths} from 'Common/Paths.jsx'
 
-import {HashRouter as Router, Route, Link} from 'react-router-dom'
+import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 
 import {ErrorBoundary} from './ErrorBoundary.jsx'
 
@@ -17,25 +17,13 @@ class App extends React.Component {
             <div>
             <ErrorBoundary>
                 <Label text="netflixroulette" />
-
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to={paths.SEARCH_PATH}>Search</Link>
-                        </li>
-                        <li>
-                            <Link to={paths.DETAILS_PATH}>Details</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                <Route path={paths.ROOT_PATH} exact component={MovieSearch} />
-                <Route path={paths.SEARCH_PATH} component={MovieSearch} />
-                <Route path={paths.DETAILS_PATH} component={MovieDetails} />
-                <Route path={paths.UNKNOWN_PATH} component={NotFound} />
+                
+                <Switch>
+                    <Route path={paths.ROOT_PATH} exact component={MovieSearch} />
+                    <Route path={paths.SEARCH_PATH} component={MovieSearch} />
+                    <Route path={paths.DETAILS_PATH} component={MovieDetails} />
+                    <Route path={paths.UNKNOWN_PATH} component={NotFound} />
+                </Switch>
 
                 <Label text="netflixroulette" />
             </ErrorBoundary>	
